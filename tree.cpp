42b63af3e1869319bcbecdf3603ca097738c8264
Cristian Canedo <ccanedo@gmx.com>
2022-01-12 16:14:26 -0500

    Use unique_ptr types with Node, implement bfs

--- tree.cpp
+++ tree.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
+#include <memory>
 
-#include "node.h"
+#include "snode.h"
 
 int main()
 {
-    Node *root = newNode(100);
+    std::unique_ptr<Node> root = newNode(100);
+    root = insert(root, 110);
+    root = insert(root, 90);
     root = insert(root, 99);
-    root = insert(root, 101);
-    root = insert(root, 98);
-    root = insert(root, 102);
     root = insert(root, 80);
+    root = insert(root, 105);
+    root = insert(root, 120);
 
-    //std::cout << "root: " << root->data << std::endl;
-    //std::cout << "root->left: " << root->left->data << std::endl;
-    //std::cout << "root->right: " << root->right->data << std::endl;
-    //std::cout << "root->left->left: " << root->left->left->data << std::endl;
-    //std::cout << "root->left->left->left: " << root->left->left->left->data << std::endl;
+    bfsLookup(root);
 
-    reversePostOrderTraversal(root, [](const Node *node) {
-            std::cout << node->data << " ";
-    });
-    std::cout << std::endl;
-
-    deleteTree(root);
     return 0;
 }
